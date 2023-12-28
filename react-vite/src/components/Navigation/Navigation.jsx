909de5e0bc431ff15f2c54e6c68ed47c674d3e45
Azadcelik <azadcelikk12@gmail.com>
2023-12-28 16:24:20 -0500

    fav-count completed

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,29 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+import { useContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getFavoriteThunk } from "../../redux/favorite";
 
 function Navigation() {
+
+const dispatch = useDispatch()
+const [count,setCount] = useState(0)
+const fav = useSelector(state => Object.values(state.favorites))
+
+
+useEffect(() => { 
+  dispatch(getFavoriteThunk())
+  setCount(fav.length)
+},[,dispatch,setCount,fav.length])
+
+
+
+  console.log('count int count', count)
   return (
     <ul>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">Home {count}</NavLink>
       </li>
 
       <li>
