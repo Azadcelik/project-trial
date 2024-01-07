ec53fecef75450ba2afe9845d8e131efcc129fa3
Azadcelik <azadcelikk12@gmail.com>
2024-01-07 10:22:45 -0500

    refactored generrally

--- react-vite/src/components/GetFavorite/GetFavorite.jsx
+++ react-vite/src/components/GetFavorite/GetFavorite.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getFavoriteThunk } from "../../redux/favorite"
-
+import './GetFavorite.css'
+import { useNavigate } from "react-router-dom"
 
 
 
@@ -9,6 +10,7 @@ import { getFavoriteThunk } from "../../redux/favorite"
 
 const GetFavorite = () => { 
 const dispatch = useDispatch()
+const navigate = useNavigate()
 
 const user = useSelector(state => state.session.user)
 const favs = useSelector(state => Object.values(state.favorites))
@@ -19,30 +21,34 @@ console.log(user,favs)
         dispatch(getFavoriteThunk())
     },[dispatch])
 
+    const handleSingleImage = (productId) => { 
+        navigate(`/product/${productId}`)
+    }
+
 return (
 
-
-    <>
-    
+<>
+        <h1 className="favorites-title">Favorited Cars</h1>
+    <div className="favorite-cars-container">
+        
         {favs.map(product => (
-         <div key={product.id}>
-            <img src={product.image} />
-            <div className="make-model-year">
-                <h3>{product.year}</h3>
-                <h3>{product.make}</h3>
-                <h3>{product.model}</h3>
-            </div>
+            <div key={product.id} className="favorite-car-card">
+                <img src={product.image} onClick={() => handleSingleImage(product.id)}/>
+                <div className="make-model-year">
+                    <h3>{product.year}</h3>
+                    <h3>{product.make}</h3>
+                    <h3>{product.model}</h3>
+                </div>
 
-            <div className="type">
-                <span>{product.type} &nbsp; &#183; &nbsp; {product.mileage} </span>
-                <span></span>
+                <div className="type">
+                    <span>{product.type} &nbsp; &#183; &nbsp; {product.mileage} miles </span>
+                </div>
+                <h2 className="price">$ {product.price}</h2>
             </div>
-            <h2 className="price">$ {product.price}</h2>
-        </div>
-            ))}
-    <h1>hey how are you doing</h1>
+        ))}
+    </div>
+</>
 
-    </>
 )
     
 }
