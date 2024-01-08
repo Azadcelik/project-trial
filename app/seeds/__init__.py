2a79028708f7fbdf8d31aa960ffb750375de805c
Azadcelik <azadcelikk12@gmail.com>
2024-01-08 13:28:40 -0500

    initial seed

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,7 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .products import seed_product, undo_product
 from app.models.db import db, environment, SCHEMA
-
+from .product_images import seed_product_images,undo_product_images
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -16,16 +16,19 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_product_images()
         undo_product()
         undo_users()
     seed_users()
     seed_product()
+    seed_product_images()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_product_images()
     undo_product()
     undo_users()
     # Add other undo functions here
